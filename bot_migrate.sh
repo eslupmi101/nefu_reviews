5d94a3e18cd3df994fe9475f6156777b2f0e4ce9
eslupmi101 <ajsen-andreev@yandex.ru>
2024-04-20 23:22:00 +0900

    Add script for creating mgrates in bot

---
+++ bot_migrate.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Проверяем, передан ли аргумент
+if [ $# -ne 1 ]; then
+    echo "Использование: $0 <ID_контейнера>"
+    exit 1
+fi
+
+# Получаем ID контейнера из аргумента
+container_id=$1
+
+# Формируем команду для выполнения в контейнере
+command="docker exec $container_id poetry run alembic upgrade head"
+
+# Выполняем команду
+echo "Выполняется команда в контейнере $container_id: $command"
+eval $command
